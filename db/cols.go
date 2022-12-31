5aa758e93d8f2f0fe30408f9086bea56c4bf9649
김창학 <sjc02183@naver.com>
2022-12-31 11:05:54 +0900

    .

--- db/cols.go
+++ db/cols.go
@@ -1,11 +1,11 @@
 package db
 
-func I_to_arri(_arri ...interface{}) (arri_ret []interface{}) {
-	arri_ret = make([]interface{}, 0, 10)
+func I_to_arri(items ...interface{}) (arrs []interface{}) {
+	arrs = make([]interface{}, 0, 10)
 
-	for _, i_item := range _arri {
-		arri_ret = append(arri_ret, i_item)
+	for _, item := range items {
+		arrs = append(arrs, item)
 	}
 
-	return arri_ret
+	return arrs
 }
--- ornn/ornn.go
+++ ornn/ornn.go
@@ -12,32 +12,32 @@ import (
 type ORNN struct {
 	db     *db.Conn
 	vendor *Vendor
-	config *config.Config
+	conf   *config.Config
 }
 
-func (t *ORNN) Init(db *db.Conn, config *config.Config) {
+func (t *ORNN) Init(db *db.Conn, conf *config.Config) {
 	t.db = db
 	t.vendor = NewVendor(db_mysql.NewVendor(db))
-	t.config = config
+	t.conf = conf
 }
 
 func (t *ORNN) ConfigLoad(path string) error {
-	if t.config == nil {
+	if t.conf == nil {
 		return fmt.Errorf("json is emtpy")
 	}
 
-	t.config.Load(path)
+	t.conf.Load(path)
 
 	return nil
 }
 
 func (t *ORNN) ConfigSave(path string) error {
 	var err error
-	if t.config == nil {
+	if t.conf == nil {
 		return fmt.Errorf("json is emtpy")
 	}
 
-	err = t.config.Save(path)
+	err = t.conf.Save(path)
 	if err != nil {
 		return err
 	}
@@ -49,7 +49,7 @@ func (t *ORNN) InitConfigBySchema(tablePrefix string) error {
 	// TODO : prefix 가 있을 시 해당 prefix 를 가지고 있는 테이블 스키마만 생성
 	// TODO : custom type 은 업데이트 되지 않음
 	// init config by schema
-	err := t.vendor.VendorBySchema(t.config)
+	err := t.vendor.VendorBySchema(t.conf)
 	if err != nil {
 		return err
 	}
@@ -57,13 +57,13 @@ func (t *ORNN) InitConfigBySchema(tablePrefix string) error {
 }
 
 func (t *ORNN) GenCode(path string) (err error) {
-	if t.config == nil {
+	if t.conf == nil {
 		return fmt.Errorf("config is emtpy")
 	}
 
 	// gen code
 	gen := &Gen{}
-	code, err := gen.Gen(t.db, t.config, path)
+	code, err := gen.Gen(t.db, t.conf, path)
 	if err != nil {
 		return err
 	}
