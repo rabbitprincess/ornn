863e66b081d244d2926798adb8ddb536621ac3ce
김창학 <sjc02183@naver.com>
2023-01-02 17:54:58 +0900

    Update cols.go

--- db/cols.go
+++ db/cols.go
@@ -1,11 +1,5 @@
 package db
 
 func I_to_arri(items ...interface{}) (arrs []interface{}) {
-	arrs = make([]interface{}, 0, 10)
-
-	for _, item := range items {
-		arrs = append(arrs, item)
-	}
-
-	return arrs
+	return items
 }
